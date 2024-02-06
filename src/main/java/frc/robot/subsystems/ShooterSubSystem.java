3b3fea636eab16b4c8fb2ad8a2ea5a71127dc50c
jashow55 <jason.howerton@gmail.com>
2024-02-05 20:48:58 -0500

    started playing with P value.  Not getting to 40 that is being commanded.  Only to 20ish. +include json file for shuffleboard

--- src/main/java/frc/robot/subsystems/ShooterSubSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubSystem.java
@@ -74,13 +74,15 @@ public class ShooterSubSystem extends SubsystemBase {
         TalonFXConfiguration configs = new TalonFXConfiguration();
 
         /* Voltage-based velocity requires a feed forward to account for the back-emf of the motor */
-        configs.Slot0.kP = 0.11; // An error of 1 rotation per second results in 2V output
+        //0.11, 0.5, 0.0001, 0.12.  , .21 got there faster,  1 was too high (oscilated around zero when off)
+        // still only getting to 20 instead of 40 that was being commanded
+        configs.Slot0.kP = .2; // An error of 1 rotation per second results in 2V output  
         configs.Slot0.kI = 0.5; // An error of 1 rotation per second increases output by 0.5V every second
         configs.Slot0.kD = 0.0001; // A change of 1 rotation per second squared results in 0.01 volts output
         configs.Slot0.kV = 0.12; // Falcon 500 is a 500kV motor, 500rpm per V = 8.333 rps per V, 1/8.33 = 0.12 volts / Rotation per second
         // Peak output of 8 volts
-        configs.Voltage.PeakForwardVoltage = 8;
-        configs.Voltage.PeakReverseVoltage = -8;
+        configs.Voltage.PeakForwardVoltage = 11;
+        configs.Voltage.PeakReverseVoltage = -11;
         
         /* Torque-based velocity does not require a feed forward, as torque will accelerate the rotor up to the desired velocity by itself */
         configs.Slot1.kP = 5; // An error of 1 rotation per second results in 5 amps output
